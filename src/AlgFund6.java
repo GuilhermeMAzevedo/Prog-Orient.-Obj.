fa5fb5b6549585d0b7b496f917d326ee8fa8f282
GuilhermeMAzevedo <guilhermemazevedo2007@gmail.com>
2025-08-21 20:32:56 -0300

    AlgFund6

---
+++ src/AlgFund6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class AlgFund6 {
+    public void fibonacci() {
+        Scanner prompt = new Scanner(System.in);
+        byte n = prompt.nextByte();
+        byte i = 0;
+        byte fia2 = 0;
+        byte fia1 = 1;
+        byte fi;
+        while (i < n){
+            System.out.print(fia2);
+            fi = (byte) (fia2 + fia1);
+            fia2 = fia1;
+            fia1 = fi;
+            if (i != (n - 1)){
+                System.out.print(", ");
+            }
+
+
+            i++;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,23 @@ public class Main {
     public static void main(String[] args) {
         //AlgFund1 a1 = new AlgFund1();
         //a1.trocaValores();
+
         //AlgFund2 a2 = new AlgFund2();
         //a2.contagem();
-<<<<<<< HEAD
 
-
-=======
         //AlgFund3 a3 = new AlgFund3();
         //a3.somador();
+
         //AlgFund4 a4 = new AlgFund4();
         //a4.fatorial();
-        AlgFund5 a5 = new AlgFund5();
-        a5.somaInfinita();
->>>>>>> a930117c5093642e7e53944b9dbc1c8c0e694161
+
+        //AlgFund5 a5 = new AlgFund5();
+        //a5.somaInfinita();
+
+        AlgFund6 a6 = new AlgFund6();
+        a6.fibonacci();
+
+
+
     }
 }
