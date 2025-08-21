80fd6c7d410297375f867e3d3f863220684f748f
GuilhermeMAzevedo <alunok10@unifor.local>
2025-08-21 15:17:24 -0300

    AlgFund 3 e 4

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
         //a1.trocaValores();
         //AlgFund2 a2 = new AlgFund2();
         //a2.contagem();
-        
+        //AlgFund3 a3 = new AlgFund3();
+        //a3.somador();
+        //AlgFund4 a4 = new AlgFund4();
+        //a4.fatorial();
 
     }
 }
